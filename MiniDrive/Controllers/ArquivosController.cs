cf1b459aa6ccef3182de6f802fe757aec812a3b7
kaique <kaiquebezerra30@gmail.com>
2025-12-16 13:20:29 -0300

    Adicionar arquivos de projeto.

---
+++ MiniDrive/Controllers/ArquivosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniDriveVideo.Data;
+using MiniDriveVideo.Models;
+
+namespace MiniDriveVideo.Controllers
+{
+    public class ArquivosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ArquivosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string tipo)
+        {
+            var arquivos = _context.Arquivos.AsQueryable();
+
+            if (!string.IsNullOrEmpty(tipo))
+            {
+                arquivos = arquivos.Where(a => a.Extensao.Contains(tipo));
+            }
+
+            ViewBag.TipoFiltro = tipo;
+
+            return View(arquivos.OrderByDescending(a => a.DataUpload).ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile arquivo)
+        {
+            if (arquivo != null && arquivo.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    arquivo.CopyTo(ms);
+
+                    var arquivoModel = new ArquivoModel
+                    {
+                        NomeArquivo = Path.GetFileNameWithoutExtension(arquivo.FileName),
+                        Extensao = Path.GetExtension(arquivo.FileName).TrimStart('.'),
+                        TipoMime = arquivo.ContentType,
+                        Tamanho = arquivo.Length,
+                        DataUpload = DateTime.UtcNow,
+                        ArquivoBytes = ms.ToArray()
+                    };
+
+                    _context.Arquivos.Add(arquivoModel);
+                    _context.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public FileResult Download(int id)
+        {
+            var arquivo = _context.Arquivos.Find(id);
+            if (arquivo == null)
+            {
+                return null;
+            }
+
+            return File(arquivo.ArquivoBytes, arquivo.TipoMime, arquivo.NomeArquivo + "." + arquivo.Extensao);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            var arquivo = _context.Arquivos.Find(id);
+            if (arquivo == null)
+            {
+                return Json(new { success = false });
+            }
+
+            _context.Arquivos.Remove(arquivo);
+            _context.SaveChanges();
+
+            return Json(new { success = true });
+        }
+    }
+}
---
+++ MiniDrive/Migrations/20251215171631_createDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MiniDriveVideo.Migrations
+{
+    /// <inheritdoc />
+    public partial class createDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Arquivos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NomeArquivo = table.Column<string>(type: "text", nullable: false),
+                    Extensao = table.Column<string>(type: "text", nullable: false),
+                    TipoMime = table.Column<string>(type: "text", nullable: false),
+                    Tamanho = table.Column<long>(type: "bigint", nullable: false),
+                    DataUpload = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ArquivoBytes = table.Column<byte[]>(type: "bytea", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Arquivos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Arquivos");
+        }
+    }
+}
